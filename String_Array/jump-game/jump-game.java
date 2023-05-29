086377b2d41da4e54a73fc308b727ed9240b492a
Jasmine(자스민) <127177959+jasmine-101@users.noreply.github.com>
2023-05-29 18:38:35 +0900

    Time: 2 ms (78.42%) | Memory: 44.2 MB (16.28%) - LeetSync

--- String_Array/jump-game/jump-game.java
+++ String_Array/jump-game/jump-game.java
@@ -4,15 +4,12 @@ class Solution {
         int max=0;
         int n=nums.length;
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=max;i++){
             max=Math.max(max,i+nums[i]);
             if(max>=n-1){
                 return true;
             }
-            if(max==i){
-                return false;
-            }
         }
-        return true;
+        return false;
     }
 }
