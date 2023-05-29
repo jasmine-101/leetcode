aae418dfea4b472701853655a06a1034ccb40041
Jasmine(자스민) <127177959+jasmine-101@users.noreply.github.com>
2023-05-29 18:09:20 +0900

    Time: 2 ms (16.70%) | Memory: 44.2 MB (23.13%) - LeetSync

--- String_Array/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ String_Array/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,36 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        Stack<Integer> stack=new Stack<>();
-        int n=prices.length;
-        stack.push(prices[0]);
+
+        int low=Integer.MAX_VALUE;
         int sum=0;
-        for(int i=1;i<n;i++){
-            int cur=prices[i];
-            if(stack.isEmpty()){
-                stack.push(cur);
-                continue;
-            }
 
-            if(stack.peek()<cur){
-                if(stack.size()>1){
-                    stack.pop();
-                }
-                stack.push(cur);
-            }else if(stack.peek()>cur){
-                int high=stack.pop();
-                if(!stack.isEmpty()){
-                    int low=stack.pop();
-                    sum+=high-low;
-                }
-                stack.push(cur);
-            }
-        }
-
-        if(stack.size()>1){
-            sum+=stack.pop()-stack.pop();
+        for(int i=0;i<prices.length;i++){
+            low=Math.min(low,prices[i]);
+            sum+=Math.max(0,prices[i]-low);
+            low=prices[i];
         }
 
         return sum;
-
     }
 }
