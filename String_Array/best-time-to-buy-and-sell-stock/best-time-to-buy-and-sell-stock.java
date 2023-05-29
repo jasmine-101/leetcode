878c510236ff8015af2af31fb7374fd1576a82c6
Jasmine(자스민) <127177959+jasmine-101@users.noreply.github.com>
2023-05-29 17:38:41 +0900

    Time: 2 ms (93.74%) | Memory: 61.4 MB (6.47%) - LeetSync

--- String_Array/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ String_Array/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int low=prices[0];
-        int high=prices[0];
         int n = prices.length;
         int answer=0;
 
         for(int i=1;i<n;i++){
             if(low>prices[i]){
                 low=prices[i];
-                high=prices[i];
-            }else if(low < prices[i]){
-                high=Math.max(high,prices[i]);
-                answer=Math.max(answer,high-low);
             }
+            answer=Math.max(answer,prices[i]-low);
         }
 
         return answer;
